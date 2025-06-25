bf56098c41c121570fecdcf9a9ed3c216e909a8e
Gabriel Silva <pedrogabriieell@gmail.com>
2025-06-25 13:56:43 -0300

    solving another exercise, forecastTemp with functions

---
+++ STUDY MATERIAL/MY - STUDY PLACE/my java script course resolutions/Developer Skills & editor setup/challenges/code challenge 2.js
@@ -0,0 +1,13 @@
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+	let finalText = "";
+	for (let i = 0; i < arr.length; i++) {
+		let temp = arr[i];
+		finalText = finalText + ("... " + temp + "ºC in " + (i + 1) + " days");
+	}
+	return finalText;
+};
+
+console.log(printForecast(data1));
