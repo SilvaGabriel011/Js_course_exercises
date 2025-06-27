8b84807b2d3513ef01202f54249de3ab39f8b9e8
Gabriel Silva <pedrogabriieell@gmail.com>
2025-06-27 00:15:24 -0300

    dom manipulation, events, understanding better elements and the relatioship between JS and HTML/CSS

--- STUDY MATERIAL/MY - STUDY PLACE/my java script course resolutions/Javascript in the browser - Dom and events/starter/script.js
+++ STUDY MATERIAL/MY - STUDY PLACE/my java script course resolutions/Javascript in the browser - Dom and events/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct number !!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!!';
+  }
+});
