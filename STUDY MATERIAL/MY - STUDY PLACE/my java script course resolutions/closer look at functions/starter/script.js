313af1436c87e71e76159a789111d31da71e307b
Gabriel Silva <pedrogabriieell@gmail.com>
2025-07-09 13:59:35 -0300

    ending the data strucutures and initializing the functions part, scope, methods of functions and closures

---
+++ STUDY MATERIAL/MY - STUDY PLACE/my java script course resolutions/closer look at functions/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// const upperFirstword = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// console.log(`${upperFirstword('Javascript is fun')}`);
+
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
+// greet('konichua')('korean doe');
